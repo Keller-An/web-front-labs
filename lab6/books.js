c0e2bbddf79fd49d55081b5c35bb27412a5388be
Махмадбекова Анастасия <nastiamah10@gmail.com>
2025-05-14 22:34:33 +0700

    Книги

---
+++ lab6/books.js
@@ -0,0 +1,114 @@
+const books = [
+    {
+        author: 'Лао-цзы',
+        name: 'Дао дэ цзин. Канон о Дао и дэ',
+        publicationYear: 2022,
+        price: 399,
+        bestseller: true,
+        available: false
+    },
+    {
+        author: 'Карл Саган',
+        name: 'Космос',
+        publicationYear: 2024,
+        price: 290,
+        bestseller: false,
+        available: true
+    },
+    {
+        author: 'Жоржи Амаду',
+        name: 'Генералы песчаных карьеров',
+        publicationYear: 1937,
+        price: 450,
+        bestseller: true,
+        available: true
+    },
+    {
+        author: 'Донна Тарт',
+        name: 'Щегол',
+        publicationYear: 2013,
+        price: 980,
+        bestseller: true,
+        available: true
+    },
+    {
+        author: 'Фёдор Михайлович Достоевский',
+        name: 'Преступление и наказание',
+        publicationYear: 1866,
+        price: 300,
+        bestseller: true,
+        available: false
+    },
+    {
+        author: 'Осаму Дадзай',
+        name: 'Человек недостойный',
+        publicationYear: 1948,
+        price: 290,
+        bestseller: false,
+        available: false
+    },
+    {
+        author: 'Рэй Брэдбери',
+        name: '451 градус по Фаренгейту',
+        publicationYear: 1953,
+        price: 390,
+        bestseller: true,
+        available: true
+    },
+    {
+        author: 'Айн Рэнд',
+        name: 'Атлант расправил плечи',
+        publicationYear: 1957,
+        price: 720,
+        bestseller: false,
+        available: true
+    },
+    {
+        author: 'Джордж Оруэлл',
+        name: '1984',
+        publicationYear: 1949,
+        price: 490,
+        bestseller: true,
+        available: true
+    },
+    {
+        author: 'Чак Паланик',
+        name: 'Бойцовский клуб',
+        publicationYear: 1996,
+        price: 378,
+        bestseller: true,
+        available: false
+    },
+    {
+        author: 'Габриэль Гарсиа Маркес',
+        name: 'Сто лет одиночества',
+        publicationYear: 1967,
+        price: 650,
+        bestseller: true,
+        available: true
+    },
+    {
+        author: 'Антуан де Сент-Экзюпери',
+        name: 'Маленький принц',
+        publicationYear: 1943,
+        price: 350,
+        bestseller: true,
+        available: true
+    },
+    {
+        author: 'Харуки Мураками',
+        name: 'Норвежский лес',
+        publicationYear: 1987,
+        price: 800,
+        bestseller: true,
+        available: false
+    },
+    {
+        author: 'И повсюду тлеют пожары',
+        name: 'Селеста Инг',
+        publicationYear: 2017,
+        price: 430,
+        bestseller: false,
+        available: true
+    }
+]
